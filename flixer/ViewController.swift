d8202f44550857199628f58ec835431c99c0a60d
vincent5779 <vncent5779@gmail.com>
2021-09-04 14:57:27 -0500

    Initial Commit

---
+++ flixer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixer
+//
+//  Created by Hew, Vincent on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ flixerTests/flixerTests.swift
@@ -0,0 +1,33 @@
+//
+//  flixerTests.swift
+//  flixerTests
+//
+//  Created by Hew, Vincent on 9/4/21.
+//
+
+import XCTest
+@testable import flixer
+
+class flixerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
