994e58bbeddbea4623e00735d8bd127b7edf7c68
vincent5779 <vncent5779@gmail.com>
2021-09-17 10:18:09 -0500

    assignmet 2 step 2
    step 2 finished

---
+++ flixer/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixer
+//
+//  Created by Hew, Vincent on 9/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
